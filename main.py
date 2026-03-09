560ffd6b96cbc6703ec7ad3724665c78382374a9
Pavel <ahpavel789@gmail.com>
2026-03-09 20:10:30 +0600

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,45 @@ def view_sessions():
         print(session["date"], "|", session["time"], "|", session["subject"], "-", session["minutes"], "mins")
 
 
-log_session("Python", 45)
-log_session("Mathematics", 60)
-log_session("GRC", 30)
-view_sessions()
+while True:
+    print("\nWhat do you want to do?")
+    print("1 - Log a session")
+    print("2 - View all sessions")
+    print("3 - Quit")
+
+    choice = input("Enter choice: ")
+
+    if choice == "1":
+        while True:
+            try:
+                count = int(input("How many subjects did you study?\n -"))
+                if count <= 0:
+                    print("Please enter a number greater than 0!")
+                    continue
+                break
+            except ValueError:
+                print("Invalid! Please enter a number only!")
+
+        for i in range(count):
+            print(f"\nSubject {i + 1}:")
+            subject = input("Subject name: ")
+            while True:
+                try:
+                    minutes = int(input("Minutes: "))
+                    if minutes <= 0:
+                        print("Please enter a number greater than 0!")
+                        continue
+                    break
+                except ValueError:
+                    print("Invalid! Please enter a number only")
+            log_session(subject, minutes)
+
+    elif choice == "2":
+        view_sessions()
+
+    elif choice == "3":
+        print("Goodbye.Keep studying")
+        break
+
+    else:
+        print("Please enter 1, 2 or 3 only!")
